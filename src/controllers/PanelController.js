83d9b43fbfc1851378198fe16da2879a34d9d227
nathanmelki <77828494+nathanmelki@users.noreply.github.com>
2024-01-23 14:19:54 +0100

    Update PanelController.js

--- src/controllers/PanelController.js
+++ src/controllers/PanelController.js
@@ -1,82 +1,15 @@
+// src/controllers/PanelController.js
 const express = require('express');
 const router = express.Router();
-const Panel = require('../models/Panel'); // Assurez-vous d'ajuster le chemin selon votre structure de projet
+const VirtualPanel = require('../models/VirtualPanel');
 
-// Endpoint pour récupérer tous les panneaux
-router.get('/', async (req, res) => {
+// Récupérer tous les panneaux virtuels
+router.get('/virtual-panels', async (req, res) => {
   try {
-    const panels = await Panel.query();
-    res.json(panels);
+    const virtualPanels = await VirtualPanel.find();
+    res.json(virtualPanels);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erreur lors de la récupération des panneaux' });
-  }
-});
-
-// Endpoint pour créer un nouveau panneau
-router.post('/', async (req, res) => {
-  const { name, location, advertiserId, videoUrl, logoUrl, timestamp } = req.body;
-
-  try {
-    const newPanel = await Panel.query().insert({
-      name,
-      location,
-      advertiserId,
-      videoUrl,
-      logoUrl,
-      timestamp,
-    });
-
-    res.status(201).json(newPanel);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erreur lors de la création du panneau' });
-  }
-});
-
-// Endpoint pour mettre à jour un panneau
-router.put('/:id', async (req, res) => {
-  const panelId = req.params.id;
-  const { name, location, advertiserId, videoUrl, logoUrl, timestamp } = req.body;
-
-  try {
-    const updatedPanel = await Panel.query().patchAndFetchById(panelId, {
-      name,
-      location,
-      advertiserId,
-      videoUrl,
-      logoUrl,
-      timestamp,
-    });
-
-    if (!updatedPanel) {
-      res.status(404).json({ error: 'Panneau non trouvé' });
-      return;
-    }
-
-    res.json(updatedPanel);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erreur lors de la mise à jour du panneau' });
-  }
-});
-
-// Endpoint pour supprimer un panneau
-router.delete('/:id', async (req, res) => {
-  const panelId = req.params.id;
-
-  try {
-    const deletedCount = await Panel.query().deleteById(panelId);
-
-    if (deletedCount === 0) {
-      res.status(404).json({ error: 'Panneau non trouvé' });
-      return;
-    }
-
-    res.status(204).end();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erreur lors de la suppression du panneau' });
+    res.status(500).json({ message: error.message });
   }
 });
 
